555391c0a40e21d63a4de6f70f467f2313e58f31
BryanRJ <66827037+bryannrj@users.noreply.github.com>
2026-02-06 11:10:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function flashRainbowColors(callback) {
         if (callback) {
             callback();
         }
-    }, 1500); // Flash colors for 2 seconds
+    }, 2000); // Flash colors for 2 seconds
 }
 
 // Function to display the cat.gif initially
@@ -76,4 +76,3 @@ function displayDuckHeart() {
 
 // Display the cat.gif initially
 displayFrostie();
-
