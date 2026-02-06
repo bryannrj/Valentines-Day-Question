e91572bacc15b88700613707a09eccff7aacd081
BryanRJ <66827037+bryannrj@users.noreply.github.com>
2026-02-06 13:43:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,21 @@ function displayFrostie() {
         imageContainer.appendChild(catImage);
     };
 }
+// Function to display the cat.gif initially
+function displayFrostieFunny() {
+    // Get the container where the image will be displayed
+    var imageContainer = document.getElementById('image-container');
+    // Create a new Image element for the cat
+    var catImage = new Image();
+    // Set the source (file path) for the cat image
+    catImage.src = 'frostie2.webp'; // Assuming the cat image is named "cat.gif"
+    // Set alternative text for the image (for accessibility)
+    catImage.alt = 'Frostie2';
+    // When the cat image is fully loaded, add it to the image container
+    catImage.onload = function() {
+        imageContainer.appendChild(catImage);
+    };
+}
 
 // Function to display the cat-heart.gif
 function displayDuckHeart() {
@@ -75,26 +90,7 @@ function displayDuckHeart() {
         document.getElementById('options').style.display = 'none';
     };
 }
-// Function to display the cat-heart.gif
-function displayFrositeFunny() {
-    // Clear existing content in the image container
-    document.getElementById('image-container').innerHTML = '';
-    // Get the container where the image will be displayed
-    var imageContainer = document.getElementById('image-container');
-    // Create a new Image element for the cat-heart
-    var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'frostie2.webp'; // Assuming the cat-heart image is named "cat-heart.gif"
-    // Set alternative text for the image (for accessibility)
-    catHeartImage.alt = 'Frostie2';
-    // When the cat-heart image is fully loaded, add it to the image container
-    catHeartImage.onload = function() {
-        imageContainer.appendChild(catHeartImage);
-        // Hide the options container
-        document.getElementById('options').style.display = 'none';
-    };
-}
-// Display the cat.gif initially
 
 displayFrostieFunny();
+
 
